2fb1fdb324bbad175e67bd682674c5d1d8a5e304
sigongjoa <zeskywa499@gmail.com>
2025-07-10 02:15:14 +0900

    fix: Ensure debug logs are visible in browser console
    Changed logger.debug to use console.log (via logger.info) in lib/logger.ts.
    This makes debug messages more consistently visible in browser developer consoles,
    as console.debug messages are sometimes filtered by default.

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,7 +1,7 @@
 export const logger = {
   debug: (...args: any[]) => {
     if (process.env.NODE_ENV === 'development') {
-      console.debug(...args);
+      console.log(...args); // Changed from console.debug to console.log
     }
   },
   info: (...args: any[]) => {
